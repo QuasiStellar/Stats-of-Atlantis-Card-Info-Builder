da1f428866e100fd59f1e413a04ecfe700fae8b4
Charlie Tran <hi@charlietran.com>
2025-02-22 17:37:25 -0600

    update brogan

--- src/main/kotlin/heroes/brogan.kt
+++ src/main/kotlin/heroes/brogan.kt
@@ -11,53 +11,51 @@ val brogan = setOf(
     Card(
         "Onslaught",
         """
-            After the attack: If the target was defeated,
-            move into the space it occupied, if able.
+            Target a unit adjacent to you. After the attack:
+            Move into the space it occupied, if able.
         """.trimIndent(),
         Color.GOLD,
         initiative = 11,
         primaryAction = Action.ATTACK,
-        primaryValue = 3,
+        primaryValue = 4,
         secondaryMovement = 1,
         secondaryDefense = 3,
     ),
     Card(
         "Leap Forward",
         """
-            Cannot target minions.
-            After the attack: If the target was defeated,
-            move into the space it occupied, if able. 
+            Target a hero adjacent to you. After the attack:
+            Move into the space it occupied, if able.
         """.trimIndent(),
         Color.GOLD,
         handicapped = true,
         initiative = 10,
         primaryAction = Action.ATTACK,
-        primaryValue = 2,
+        primaryValue = 3,
         secondaryMovement = 1,
         secondaryDefense = 3,
     ),
     Card(
         "Bulwark",
         """
-            **Choose one —**
-            >>**This round:** You and friendly units in radius
-            >cannot be moved, pushed, swapped, or
-            >forced to move by enemy heroes.
-            >>If your discard is empty, retrieve this card.
+            You may retrieve a discarded card.
+            **This turn:** You and friendly units in radius
+            cannot be moved, pushed, swapped
+            or placed by enemy heroes.
         """.trimIndent(),
         Color.SILVER,
-        initiative = 5,
+        initiative = 12,
         primaryAction = Action.SKILL,
         modifier = Modifier.AREA,
-        modifierValue = 2,
+        modifierValue = 4,
         secondaryDefense = 4,
     ),
     Card(
         "One Man Army",
         """
-            You count as 2 minions during minion battle.
-            If you would be removed during minion
-            battle, you lose the push instead.
+            During minion battle you count as a heavy
+            minion; if you would be removed,
+            lose the push instead.
         """.trimIndent(),
         Color.PURPLE,
         level = 4,
@@ -65,24 +63,23 @@ val brogan = setOf(
     Card(
         "Brutal Jab",
         """
-            **Choose one —**
-            >>Remove a token adjacent to you.
-            >>Push an enemy minion adjacent to you
-            >up to 2 spaces.
+            You may move 1 space. Push an enemy unit
+            or a token adjacent to you up to 1 space.
         """.trimIndent(),
         Color.BLUE,
         level = 1,
-        initiative = 9,
+        initiative = 8,
         primaryAction = Action.SKILL,
         secondaryMovement = 2,
-        secondaryDefense = 6,
+        secondaryDefense = 5,
     ),
     Card(
         "Mad Dash",
         """
-            Before the attack:
-            Move 2 spaces in a straight line.
-            ~(If you cannot make this move, you cannot attack).
+            Before the attack: Move 2 spaces
+            in a straight line to a space adjacent to
+            an enemy unit, then target that unit.
+            ~(If you cannot make this move, you cannot attack.)
         """.trimIndent(),
         Color.RED,
         level = 1,
@@ -95,26 +92,25 @@ val brogan = setOf(
     Card(
         "Shield",
         """
-            **This round:** If a friendly melee minion in
-            radius would be defeated, you may discard
-            a card. If you do, the minion is not defeated.
+            **This round:** When any friendly minion in radius
+            is defeated you may discard a silver card.
+            If you do, the minion is not removed.
+            ~(The enemy hero still gains the coins for defeating the minion.)
         """.trimIndent(),
         Color.GREEN,
         level = 1,
-        initiative = 5,
+        initiative = 6,
         primaryAction = Action.SKILL,
         modifier = Modifier.AREA,
-        modifierValue = 2,
+        modifierValue = 1,
         secondaryMovement = 2,
         secondaryDefense = 4,
     ),
     Card(
-        "Crushing Punch",
+        "Mighty Punch",
         """
-            Move up to 1 space, then **Choose one —**
-            >>Remove a token adjacent to you.
-            >>Push an enemy minion adjacent to you up
-            >to 2 spaces.
+            You may move 1 space. Push an enemy unit
+            or a token adjacent to you up to 2 spaces.
         """.trimIndent(),
         Color.BLUE,
         level = 2,
@@ -122,14 +118,15 @@ val brogan = setOf(
         initiative = 9,
         primaryAction = Action.SKILL,
         secondaryMovement = 2,
-        secondaryDefense = 7,
-        item = Item.ATTACK,
+        secondaryDefense = 6,
+        item = Item.INITIATIVE,
     ),
     Card(
         "Bullrush",
         """
-            Before the attack: Move 2 or 3
-            spaces in a straight line.
+            Before the attack: Move 2 or 3 spaces in
+            a straight line to a space adjacent to an
+            enemy unit, then target that unit.
         """.trimIndent(),
         Color.RED,
         level = 2,
@@ -144,9 +141,9 @@ val brogan = setOf(
     Card(
         "Bolster",
         """
-            **This round:** If a friendly non-heavy minion in
-            radius would be defeated, you may discard a
-            card. If you do, the minion is not defeated.
+            **This round:** When any friendly minion in radius
+            is defeated you may discard a silver card.
+            If you do, the minion is not removed.
         """.trimIndent(),
         Color.GREEN,
         level = 2,
@@ -157,12 +154,12 @@ val brogan = setOf(
         modifierValue = 2,
         secondaryMovement = 2,
         secondaryDefense = 5,
-        item = Item.INITIATIVE,
+        item = Item.ATTACK,
     ),
     Card(
         "Shield Bash",
         """
-            An enemy hero adjacent to you, who
+            An enemy hero adjacent to you who
             has played an attack card this turn
             discards a card, if able.
         """.trimIndent(),
@@ -172,14 +169,16 @@ val brogan = setOf(
         initiative = 9,
         primaryAction = Action.SKILL,
         secondaryMovement = 2,
-        secondaryDefense = 7,
-        item = Item.DEFENSE,
+        secondaryDefense = 6,
+        item = Item.ATTACK,
     ),
     Card(
         "Throwing Axe",
         """
-            Before the attack: You may discard a card.
-            If you do, +2 ::range_red:: Range.
+            **Choose one —**
+            >>Target a hero adjacent to you.
+            >>You may discard a card;
+            if you do, target a unit in range.
         """.trimIndent(),
         Color.RED,
         level = 2,
@@ -188,18 +187,17 @@ val brogan = setOf(
         primaryAction = Action.ATTACK,
         primaryValue = 3,
         modifier = Modifier.RANGE,
-        modifierValue = 1,
-        modifierValueSign = Sign.PLUS,
+        modifierValue = 2,
         secondaryMovement = 4,
-        secondaryDefense = 6,
+        secondaryDefense = 7,
         item = Item.DEFENSE,
     ),
     Card(
         "War Drummer",
         """
-            A friendly hero in range may retrieve
-            a discarded card if either you, or that
-            hero, is adjacent to an enemy unit.
+            A friendly hero in range gains 1 coin;
+            if any hero was defeated this round,
+            that friendly hero gains 3 coins instead.
         """.trimIndent(),
         Color.GREEN,
         level = 2,
@@ -207,33 +205,32 @@ val brogan = setOf(
         initiative = 5,
         primaryAction = Action.SKILL,
         modifier = Modifier.RANGE,
-        modifierValue = 3,
+        modifierValue = 4,
         secondaryMovement = 2,
         secondaryDefense = 5,
-        item = Item.ATTACK,
+        item = Item.DEFENSE,
     ),
     Card(
         "Savage Kick",
         """
-            Move up to 1 space, then **Choose one —**
-            >>Remove a token adjacent to you.
-            >>Push an enemy unit adjacent to you up to
-            >2 spaces.
+            Move up to 2 spaces. Push an enemy unit
+            or a token adjacent to you up to 2 spaces.
         """.trimIndent(),
         Color.BLUE,
         level = 3,
         variant = 1 to 2,
-        initiative = 10,
+        initiative = 9,
         primaryAction = Action.SKILL,
         secondaryMovement = 2,
-        secondaryDefense = 7,
-        item = Item.ATTACK,
+        secondaryDefense = 6,
+        item = Item.INITIATIVE,
     ),
     Card(
         "Furious Charge",
         """
-            Before the attack: Move 2, 3 or 4
-            spaces in a straight line.
+            Before the attack: Move 2, 3 or 4 spaces
+            in a straight line to a space adjacent to an
+            enemy unit, then target that unit.
         """.trimIndent(),
         Color.RED,
         level = 3,
@@ -248,42 +245,44 @@ val brogan = setOf(
     Card(
         "Fortify",
         """
-            **This round:** If a friendly minion in radius
-            would be defeated, you may discard a card.
-            If you do, the minion is not defeated.
+            **This round:** When any friendly minion in radius
+            is defeated you may discard a basic card.
+            If you do, the minion is not removed.
         """.trimIndent(),
         Color.GREEN,
         level = 3,
         variant = 1 to 2,
-        initiative = 4,
+        initiative = 5,
         primaryAction = Action.SKILL,
         modifier = Modifier.AREA,
         modifierValue = 2,
         secondaryMovement = 2,
         secondaryDefense = 5,
-        item = Item.INITIATIVE,
+        item = Item.ATTACK,
     ),
     Card(
         "Counterattack",
         """
-            An enemy hero adjacent to you, who
+            An enemy hero adjacent to you who
             has played an attack card this turn
             discards a card, or is defeated.
         """.trimIndent(),
         Color.BLUE,
         level = 3,
         variant = 2 to 2,
-        initiative = 10,
+        initiative = 9,
         primaryAction = Action.SKILL,
         secondaryMovement = 2,
-        secondaryDefense = 7,
+        secondaryDefense = 6,
         item = Item.RANGE,
     ),
     Card(
         "Throwing Spear",
         """
-            Before the attack: You may discard a card.
-            +2 ::range_red:: Range if you have a card in the discard.
+            **Choose one —**
+            >>Target a hero adjacent to you.
+            >>You may discard a card. If you have a card
+            in the discard, target a unit in range.
         """.trimIndent(),
         Color.RED,
         level = 3,
@@ -291,6 +290,8 @@ val brogan = setOf(
         initiative = 8,
         primaryAction = Action.ATTACK,
         primaryValue = 4,
+        modifier = Modifier.RANGE,
+        modifierValue = 2,
         secondaryMovement = 4,
         secondaryDefense = 6,
         item = Item.AREA,
@@ -298,17 +299,17 @@ val brogan = setOf(
     Card(
         "Master Skald",
         """
-            A friendly hero in range may retrieve a
-            resolved or discarded card if either you,
-            or that hero, is adjacent to an enemy unit.
+            A friendly hero in range gains 2 coin2;
+            if any hero was defeated this round,
+            that friendly hero gains 4 coins instead.
         """.trimIndent(),
         Color.GREEN,
         level = 3,
         variant = 2 to 2,
-        initiative = 4,
+        initiative = 5,
         primaryAction = Action.SKILL,
         modifier = Modifier.RANGE,
-        modifierValue = 3,
+        modifierValue = 4,
         secondaryMovement = 2,
         secondaryDefense = 5,
         item = Item.DEFENSE,
