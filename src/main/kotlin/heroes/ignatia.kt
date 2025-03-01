3c51f96b5b7c2a2390ca7fa718aac255f4bc11f6
Charlie Tran <hi@charlietran.com>
2025-03-01 02:35:35 -0600

    ignatia

--- src/main/kotlin/heroes/ignatia.kt
+++ src/main/kotlin/heroes/ignatia.kt
@@ -8,17 +8,17 @@ import Modifier
 
 val ignatia = setOf(
     Card(
-        "Chaos Bolt",
+        "Unreliable Jolt",
         """
-            ::tiebreaker_blue::: Target a non-adjacent minion in range.
+            ::tiebreaker_blue::: Target a minion adjacent to you.
             ---
-            ::tiebreaker_orange:: Target a non-adjacent hero in range.
-            ~(Effect is based on the visible side of the Tie Breaker coin).
+            ::tiebreaker_orange:: Target a hero in range.
         """.trimIndent(),
         Color.GOLD,
+        handicapped = true,
         initiative = 11,
         primaryAction = Action.ATTACK,
-        primaryValue = 3,
+        primaryValue = 2,
         modifier = Modifier.RANGE,
         modifierValue = 3,
         secondaryMovement = 1,
@@ -42,23 +42,24 @@ val ignatia = setOf(
         secondaryDefense = 1,
     ),
     Card(
-        "Moment of Equilibrium",
+        "Equilibrium",
         """
-            Flip the Tie Breaker coin to a side of your
-            choice. **This turn and next turn:**
-            The coin is not flipped after a tie.
+            **This round:** Each time you perform or repeat
+            a primary action, you may apply either ::tiebreaker_blue:: or ::tiebreaker_orange::
+            card text, regardless of the Tie Breaker coin.
         """.trimIndent(),
         Color.SILVER,
-        initiative = 12,
+        initiative = 1,
         primaryAction = Action.SKILL,
-        secondaryDefense = 3,
+        secondaryDefense = 7,
     ),
     Card(
         "Chaos Incarnate",
         """
-            After you perform a primary action, you may
-            flip the Tie Breaker token to the other side
-            and repeat that action once, if able.
+            The first time each turn after youy perform
+            a primary actionm you may flip the Tie Breaker
+            coin; if you do, you may perform that action
+            again, choosing different targets.
         """.trimIndent(),
         Color.PURPLE,
         level = 4,
@@ -67,36 +68,36 @@ val ignatia = setOf(
         "Abrupt Combustion",
         """
             ::tiebreaker_blue::: An enemy hero in radius, adjacent to a
-            token, or a minion, discards a card, If able.
+            token or a minion discards a card, if able.
             ---
-            ::tiebreaker_orange:: An adjacent enemy hero
-            discards a card, if able.
+            ::tiebreaker_orange:: Remove an enemy minion in radius
+            adjacent to an enemy hero.
         """.trimIndent(),
         Color.BLUE,
         level = 1,
         initiative = 9,
         primaryAction = Action.SKILL,
         modifier = Modifier.AREA,
-        modifierValue = 2,
-        secondaryMovement = 2,
-        secondaryDefense = 4,
+        modifierValue = 3,
+        secondaryMovement = 3,
+        secondaryDefense = 3,
     ),
     Card(
         "Playing with Fire",
         """
-            ::tiebreaker_blue::: Target a hero in a straight line.
+            ::tiebreaker_blue::: Target a unit in range not in a straight line.
             ---
-            ::tiebreaker_orange:: Target a hero not in a straight line.
+            ::tiebreaker_orange:: Target a unit in range in a straight line.
         """.trimIndent(),
         Color.RED,
         level = 1,
         initiative = 7,
         primaryAction = Action.ATTACK,
-        primaryValue = 4,
+        primaryValue = 3,
         modifier = Modifier.RANGE,
-        modifierValue = 3,
-        secondaryMovement = 4,
-        secondaryDefense = 5,
+        modifierValue = 2,
+        secondaryMovement = 3,
+        secondaryDefense = 4,
     ),
     Card(
         "Path of Ashes",
@@ -105,8 +106,7 @@ val ignatia = setOf(
             Place a ::token_magma:: magma token in each empty
             space you moved through, or out of.
             ---
-            ::tiebreaker_orange:: Place up to 2 magma tokens in radius
-            adjacent to another magma token, if able.
+            ::tiebreaker_orange:: Place up to 2 magma tokens in radius.
         """.trimIndent(),
         Color.GREEN,
         level = 1,
@@ -115,16 +115,16 @@ val ignatia = setOf(
         modifier = Modifier.AREA,
         modifierValue = 3,
         secondaryMovement = 2,
-        secondaryDefense = 3,
+        secondaryDefense = 2,
     ),
     Card(
         "Spontaneous Immolation",
         """
-            ::tiebreaker_blue::: An enemy hero in radius, adjacent to a
-            token, or a minion, discards a card, If able.
+            ::tiebreaker_blue::: An enemy hero in radius adjacent to a
+            token or a minion discards a card, if able.
             ---
-            ::tiebreaker_orange:: An adjacent enemy hero
-            discards a card, if able.
+            ::tiebreaker_orange:: Remove an enemy minion in radius
+            adjacent to an enemy hero.
         """.trimIndent(),
         Color.BLUE,
         level = 2,
@@ -132,28 +132,67 @@ val ignatia = setOf(
         initiative = 9,
         primaryAction = Action.SKILL,
         modifier = Modifier.AREA,
-        modifierValue = 3,
-        secondaryMovement = 2,
-        secondaryDefense = 5,
+        modifierValue = 4,
+        secondaryMovement = 3,
+        secondaryDefense = 4,
         item = Item.ATTACK,
+    ),
+    Card(
+        "Searing Heat",
+        """
+            ::tiebreaker_blue::: Move a friendly hero in radius
+            2 spaces in a straight line.
+            ---
+            ::tiebreaker_orange:: Move an enemy hero in radius
+            2 spaces in a straight line.
+        """.trimIndent(),
+        Color.BLUE,
+        level = 2,
+        variant = 2 to 2,
+        initiative = 9,
+        primaryAction = Action.SKILL,
+        modifier = Modifier.AREA,
+        modifierValue = 3,
+        secondaryMovement = 3,
+        secondaryDefense = 4,
+        item = Item.INITIATIVE,
     ),
     Card(
         "Erratic Fireblast",
         """
-            ::tiebreaker_blue::: Target a unit in a straight line.
+            ::tiebreaker_blue::: Target a unit in range not in a straight line.
             ---
-            ::tiebreaker_orange:: Target a unit not in a straight line.
+            ::tiebreaker_orange:: Target a unit in range in a straight line.
         """.trimIndent(),
         Color.RED,
         level = 2,
         variant = 1 to 2,
         initiative = 7,
         primaryAction = Action.ATTACK,
-        primaryValue = 5,
+        primaryValue = 3,
         modifier = Modifier.RANGE,
         modifierValue = 3,
-        secondaryMovement = 4,
+        secondaryMovement = 3,
         secondaryDefense = 5,
+        item = Item.DEFENSE,
+    ),
+    Card(
+        "Crack of Doom",
+        """
+            ::tiebreaker_blue:: Target a unit adjacent to you.
+            ---
+            ::tiebreaker_orange::: Target a unit at a maximum range.
+        """.trimIndent(),
+        Color.RED,
+        level = 2,
+        variant = 2 to 2,
+        initiative = 7,
+        primaryAction = Action.ATTACK,
+        primaryValue = 5,
+        modifier = Modifier.RANGE,
+        modifierValue = 5,
+        secondaryMovement = 3,
+        secondaryDefense = 6,
         item = Item.INITIATIVE,
     ),
     Card(
@@ -163,8 +202,7 @@ val ignatia = setOf(
             Place a ::token_magma:: magma token in each empty
             space you moved through, or out of.
             ---
-            ::tiebreaker_orange:: Place up to 3 magma tokens in radius
-            adjacent to another magma token, if able.
+            ::tiebreaker_orange:: Place up to 3 magma tokens in radius.
         """.trimIndent(),
         Color.GREEN,
         level = 2,
@@ -172,56 +210,17 @@ val ignatia = setOf(
         initiative = 5,
         primaryAction = Action.SKILL,
         modifier = Modifier.AREA,
-        modifierValue = 3,
+        modifierValue = 4,
         secondaryMovement = 2,
-        secondaryDefense = 4,
-        item = Item.INITIATIVE,
-    ),
-    Card(
-        "Searing Heat",
-        """
-            ::tiebreaker_blue::: Move an adjacent enemy
-            unit up to 3 spaces.
-            ---
-            ::tiebreaker_orange:: Move a non-adjacent enemy
-            unit in radius up to 1 spaces.
-        """.trimIndent(),
-        Color.BLUE,
-        level = 2,
-        variant = 2 to 2,
-        initiative = 9,
-        primaryAction = Action.SKILL,
-        modifier = Modifier.AREA,
-        modifierValue = 3,
-        secondaryMovement = 2,
-        secondaryDefense = 5,
-        item = Item.DEFENSE,
-    ),
-    Card(
-        "Crack of Doom",
-        """
-            ::tiebreaker_blue::: Target a unit at a maximum range.
-            ---
-            ::tiebreaker_orange:: Target an adjacent unit.
-        """.trimIndent(),
-        Color.RED,
-        level = 2,
-        variant = 2 to 2,
-        initiative = 7,
-        primaryAction = Action.ATTACK,
-        primaryValue = 6,
-        modifier = Modifier.RANGE,
-        modifierValue = 2,
-        secondaryMovement = 4,
-        secondaryDefense = 7,
+        secondaryDefense = 2,
         item = Item.DEFENSE,
     ),
     Card(
         "Unstable Portal",
         """
-            ::tiebreaker_blue::: You swap with a friendly unit in radius.
+            ::tiebreaker_blue::: Swap with a friendly unit in radius.
             ---
-            ::tiebreaker_orange:: You swap with an enemy unit in radius.
+            ::tiebreaker_orange:: Swap with an enemy unit in radius.
         """.trimIndent(),
         Color.GREEN,
         level = 2,
@@ -229,51 +228,90 @@ val ignatia = setOf(
         initiative = 5,
         primaryAction = Action.SKILL,
         modifier = Modifier.AREA,
-        modifierValue = 3,
+        modifierValue = 4,
         secondaryMovement = 2,
-        secondaryDefense = 4,
+        secondaryDefense = 2,
         item = Item.ATTACK,
     ),
     Card(
         "Violent Conflagration",
         """
-            ::tiebreaker_blue::: An enemy hero in radius,
-            adjacent to a token, or a minion,
-            discards a card, or is defeated.
+            ::tiebreaker_blue::: An enemy hero in radius adjacent to a
+            token or a minion discards a card, or is defeated.
             ---
-            ::tiebreaker_orange:: An adjacent enemy hero
-            discards a card, or is defeated.
+            ::tiebreaker_orange:: Defeat an enemy minion in radius
+            adjacent to an enemy hero.
         """.trimIndent(),
         Color.BLUE,
-        level = 3,
+        level = 2,
         variant = 1 to 2,
         initiative = 10,
         primaryAction = Action.SKILL,
         modifier = Modifier.AREA,
-        modifierValue = 3,
-        secondaryMovement = 2,
-        secondaryDefense = 5,
+        modifierValue = 4,
+        secondaryMovement = 3,
+        secondaryDefense = 4,
         item = Item.RANGE,
+    ),
+    Card(
+        "Scorching Blaze",
+        """
+            ::tiebreaker_blue::: Move a friendly hero in radius
+            2 or 3 spaces in a straight line.
+            ---
+            ::tiebreaker_orange:: Move an enemy hero in radius
+            2 or 3 spaces in a straight line.
+        """.trimIndent(),
+        Color.BLUE,
+        level = 2,
+        variant = 2 to 2,
+        initiative = 10,
+        primaryAction = Action.SKILL,
+        modifier = Modifier.AREA,
+        modifierValue = 3,
+        secondaryMovement = 3,
+        secondaryDefense = 4,
+        item = Item.MOVEMENT,
     ),
     Card(
         "Loosely-Aimed Firebolts",
         """
-            ::tiebreaker_blue::: Target a unit in a straight line.
-            Repeat once on a different hero.
+            ::tiebreaker_blue::: Target a unit in range not in a straight line.
             ---
-            ::tiebreaker_orange:: Target a unit not in a straight line.
+            ::tiebreaker_orange:: Target a unit in range in a straight line.
+            May repeat once on a different hero.
         """.trimIndent(),
         Color.RED,
         level = 3,
         variant = 1 to 2,
         initiative = 8,
         primaryAction = Action.ATTACK,
-        primaryValue = 5,
+        primaryValue = 4,
         modifier = Modifier.RANGE,
         modifierValue = 3,
-        secondaryMovement = 4,
+        secondaryMovement = 3,
+        secondaryDefense = 5,
+        item = Item.AREA,
+    ),
+    Card(
+        "Imminent Eruption",
+        """
+            ::tiebreaker_blue::: Target a unit adjacent to you.
+            May repeat once on a minion.
+            ---
+            ::tiebreaker_orange:: Target a unit at maximum range.
+        """.trimIndent(),
+        Color.RED,
+        level = 3,
+        variant = 2 to 2,
+        initiative = 8,
+        primaryAction = Action.ATTACK,
+        primaryValue = 6,
+        modifier = Modifier.RANGE,
+        modifierValue = 5,
+        secondaryMovement = 3,
         secondaryDefense = 6,
-        item = Item.MOVEMENT,
+        item = Item.INITIATIVE,
     ),
     Card(
         "Path of Flames",
@@ -282,8 +320,7 @@ val ignatia = setOf(
             Place a ::token_magma:: magma token in each empty
             space you moved through, or out of.
             ---
-            ::tiebreaker_orange:: Place up to 4 magma tokens in radius
-            adjacent to another magma token, if able.
+            ::tiebreaker_orange:: Place up to 4 magma tokens in radius.
         """.trimIndent(),
         Color.GREEN,
         level = 3,
@@ -291,67 +328,29 @@ val ignatia = setOf(
         initiative = 4,
         primaryAction = Action.SKILL,
         modifier = Modifier.AREA,
-        modifierValue = 3,
+        modifierValue = 4,
         secondaryMovement = 2,
-        secondaryDefense = 4,
-        item = Item.INITIATIVE,
-    ),
-    Card(
-        "Scorching Blaze",
-        """
-            ::tiebreaker_blue::: Move an adjacent enemy unit up to 4
-            spaces. Repeat once on a different target.
-            ---
-            ::tiebreaker_orange:: Move a non-adjacent enemy
-            unit in radius up to 2 spaces.
-        """.trimIndent(),
-        Color.BLUE,
-        level = 3,
-        variant = 2 to 2,
-        initiative = 10,
-        primaryAction = Action.SKILL,
-        modifier = Modifier.AREA,
-        modifierValue = 3,
-        secondaryMovement = 2,
-        secondaryDefense = 5,
-        item = Item.ATTACK,
-    ),
-    Card(
-        "Imminent Eruption",
-        """
-            ::tiebreaker_blue::: Target a unit at a maximum range.
-            ---
-            ::tiebreaker_orange:: Target an adjacent unit.
-            Repeat once on a minion.
-        """.trimIndent(),
-        Color.RED,
-        level = 3,
-        variant = 2 to 2,
-        initiative = 8,
-        primaryAction = Action.ATTACK,
-        primaryValue = 6,
-        modifier = Modifier.RANGE,
-        modifierValue = 2,
-        secondaryMovement = 4,
-        secondaryDefense = 8,
-        item = Item.AREA,
+        secondaryDefense = 3,
+        item = Item.DEFENSE,
     ),
     Card(
         "Chaos Gate",
         """
-            ::tiebreaker_blue::: You swap with a friendly unit in radius.
+            ::tiebreaker_blue::: Swap with a friendly unit in radius.
+            You may move that unit 1 space.
             ---
-            ::tiebreaker_orange:: You swap with an enemy unit in radius.
+            ::tiebreaker_orange:: Swap with an enemy unit in radius.
+            You may move 1 space.
         """.trimIndent(),
         Color.GREEN,
         level = 3,
         variant = 2 to 2,
         initiative = 4,
-        primaryAction = Action.DEFENSE_SKILL,
-        primaryValue = 4,
+        primaryAction = Action.SKILL,
         modifier = Modifier.AREA,
-        modifierValue = 3,
+        modifierValue = 4,
         secondaryMovement = 2,
-        item = Item.DEFENSE,
+        secondaryDefense = 3,
+        item = Item.ATTACK,
     ),
 )
