30f4296495386f399993cf333c88b2275236aacc
Charlie Tran <hi@charlietran.com>
2025-03-02 10:19:42 -0600

    nebkher

--- src/main/kotlin/heroes/nebkher.kt
+++ src/main/kotlin/heroes/nebkher.kt
@@ -11,18 +11,17 @@ val nebkher = setOf(
         "Mind Grip",
         """
             **Choose one —**
-            >>Perform the primary action of the card in
-            >the previous turn slot of an enemy hero in
-            >range; apply no item bonuses. If you would
-            >place a token, place an illusion token
-            >instead. Ignore effects that give markers.
+            >>Perform an action on the card in the previous
+            >turn slot of an enemy hero in range; if you
+            >would place any tokens, place ::token_illusion::
+            >Illusion tokens instead; skip giving markers.
             >>Defeat a minion adjacent to you.
         """.trimIndent(),
         Color.GOLD,
         initiative = 9,
         primaryAction = Action.SKILL,
         modifier = Modifier.RANGE,
-        modifierValue = 4,
+        modifierValue = 5,
         secondaryMovement = 1,
         secondaryDefense = 2,
     ),
@@ -31,7 +30,7 @@ val nebkher = setOf(
         """
             Perform the action on the card in the previous
             turn slot of an enemy hero in range; if you
-            would place any tokens this way, place a ::token_illusion::
+            would place any tokens this way, place ::token_illusion::
             Illusion tokens instead; do not use markers.
         """.trimIndent(),
         Color.GOLD,
@@ -46,109 +45,126 @@ val nebkher = setOf(
     Card(
         "Diabolical Laughter",
         """
-            Laugh diabolically. If you do, **choose one —**
-            >>You swap with an illusion token in radius.
-            >>You swap with a friendly minion in radius.
-            >>You are defeated. Enemies gain no coins.
-            >>**Next turn:** Gain +2 ::initiative:: Initiative.
+            Laugh diabolically;
+            if you do, **choose up to three times —**
+            >>Swap with an ::token_illusion:: Illusion token in radius.
+            >>Place an Illusion token in an adjacent space.
+            >>Swap two resolved cards of an enemy hero
+            >in radius, without canceling active effects.
         """.trimIndent(),
         Color.SILVER,
-        initiative = 12,
+        initiative = 3,
         primaryAction = Action.SKILL,
         modifier = Modifier.AREA,
         modifierValue = 4,
-        secondaryDefense = 4,
+        secondaryDefense = 3,
     ),
     Card(
         "What the Hell Are You?",
         """
-            If you would be defeated as part of your own
-            action, first defeat all enemy minions in radius
-            and enemy heroes in radius discard a card, or
-            are defeated. Spend no life counters.
+            Each time after you laugh diabolically as part
+            of performing an action, all enemy heroes
+            in radius discard a card, or are defeated.
         """.trimIndent(),
         Color.PURPLE,
         modifier = Modifier.AREA,
-        modifierValue = 2,
+        modifierValue = 5,
         level = 4,
     ),
     Card(
         "Imbue Doubt",
         """
-            A hero in radius must swap their unresolved
-            card with another card in their hand, if able.
+            Name a color. **Next turn, after playing cards:**
+            An enemy hero in radius discards
+            a card of that color, if able.
         """.trimIndent(),
         Color.BLUE,
         level = 1,
-        initiative = 9,
+        initiative = 10,
         primaryAction = Action.SKILL,
         modifier = Modifier.AREA,
         modifierValue = 3,
         secondaryMovement = 2,
-        secondaryDefense = 5,
+        secondaryDefense = 4,
     ),
     Card(
-        "Assume Control",
+        "Phantasmal Sentry",
         """
             **Choose one —**
-            >>Before the attack: Move a friendly minion in
-            >range up to 3 spaces to a space adjacent to
-            >an enemy hero in range. Target that hero.
-            >>Target a minion adjacent to you.
+            >>Target a hero in range adjacent
+            >to an ::token_illusion:: Illusion token in range.
+            >>Target a unit adjacent to you.
         """.trimIndent(),
         Color.RED,
         level = 1,
         initiative = 7,
         primaryAction = Action.ATTACK,
-        primaryValue = 3,
+        primaryValue = 2,
         modifier = Modifier.RANGE,
-        modifierValue = 2,
+        modifierValue = 4,
         secondaryMovement = 3,
-        secondaryDefense = 7,
+        secondaryDefense = 6,
     ),
     Card(
         "Fleeting Image",
         """
-            Place an ::token_illusion:: illusion token in radius.
-            **This round:** After an action makes you
-            discard a card, swap with an illusion token,
-            regardless of radius, then remove that token.
-            ~(Complete the action causing the discard before the swap).
+            Place an ::token_illusion:: Illusion token in radius.
+            You may swap with an Illusion token in play.
         """.trimIndent(),
         Color.GREEN,
         level = 1,
         initiative = 6,
         primaryAction = Action.SKILL,
         modifier = Modifier.AREA,
-        modifierValue = 3,
+        modifierValue = 2,
         secondaryMovement = 2,
-        secondaryDefense = 4,
+        secondaryDefense = 3,
     ),
     Card(
         "Time to Reconsider",
         """
-            A hero in radius must swap their unresolved
-            card with another card in their hand, if able.
+            Name a color. **Next turn, after playing cards:**
+            An enemy hero in radius discards
+            a card of that color, if able.
         """.trimIndent(),
         Color.BLUE,
         level = 2,
         variant = 1 to 2,
-        initiative = 10,
+        initiative = 11,
         primaryAction = Action.SKILL,
         modifier = Modifier.AREA,
         modifierValue = 4,
         secondaryMovement = 2,
-        secondaryDefense = 6,
+        secondaryDefense = 5,
+        item = Item.ATTACK,
+    ),
+    Card(
+        "Crack in Reality",
+        """
+            Split the board into two sides with a straight
+            line of spaces drawn through your space.
+            **This turn:** Units on either side of the line
+            cannot interact with objects and spaces on the
+            other side of the line, as if they did not exist.
+            ~(This includes movement, placement, radius-based effects, etc.)
+        """.trimIndent(),
+        Color.BLUE,
+        level = 2,
+        variant = 2 to 2,
+        initiative = 11,
+        primaryAction = Action.SKILL,
+        secondaryMovement = 2,
+        secondaryDefense = 5,
         item = Item.DEFENSE,
     ),
     Card(
-        "Direct Control",
+        "Phantasmal Warrior",
         """
             **Choose one —**
-            >>Before the attack: Move a friendly minion in
-            >range up to 4 spaces to a space adjacent to
-            >an enemy hero in range. Target that hero.
-            >>Target a minion adjacent to you.
+            >>Before the attack: Move an ::token_illusion:: Illusion token
+            >in range up to 1 space to a space adjacent
+            >to an enemy hero in range. Target that hero.
+            >>Target a unit adjacent to you.
         """.trimIndent(),
         Color.RED,
         level = 2,
@@ -157,54 +173,17 @@ val nebkher = setOf(
         primaryAction = Action.ATTACK,
         primaryValue = 3,
         modifier = Modifier.RANGE,
-        modifierValue = 3,
+        modifierValue = 5,
         secondaryMovement = 3,
-        secondaryDefense = 8,
+        secondaryDefense = 7,
         item = Item.INITIATIVE,
-    ),
-    Card(
-        "Multiple Projections",
-        """
-            Place up to 2 ::token_illusion:: illusion tokens in radius.
-            **This round:** After an action makes you
-            discard a card, swap with an illusion token,
-            regardless of radius, then remove that token.
-        """.trimIndent(),
-        Color.GREEN,
-        level = 2,
-        variant = 1 to 2,
-        initiative = 5,
-        primaryAction = Action.SKILL,
-        modifier = Modifier.AREA,
-        modifierValue = 3,
-        secondaryMovement = 2,
-        secondaryDefense = 4,
-        item = Item.INITIATIVE,
-    ),
-    Card(
-        "Crack in Reality",
-        """
-            Split the board with a straight line of spaces
-            drawn through your space: **This turn:**
-            Units on either side of the line are immune to
-            actions of units on the opposite side of the line.
-        """.trimIndent(),
-        Color.BLUE,
-        level = 2,
-        variant = 2 to 2,
-        initiative = 10,
-        primaryAction = Action.SKILL,
-        secondaryMovement = 2,
-        secondaryDefense = 6,
-        item = Item.ATTACK,
     ),
     Card(
         "Twist Fate",
         """
             Target a unit in range. After the attack:
-            If you target a hero, you may swap any
-            two of their resolved cards.
-            ~(Any active effects on the swapped cards are canceled).
+            You may swap an enemy unit in range
+            with an ::token_illusion:: Illusion token adjacent to you.
         """.trimIndent(),
         Color.RED,
         level = 2,
@@ -215,15 +194,33 @@ val nebkher = setOf(
         modifier = Modifier.RANGE,
         modifierValue = 2,
         secondaryMovement = 3,
-        secondaryDefense = 7,
+        secondaryDefense = 6,
         item = Item.DEFENSE,
     ),
     Card(
-        "Phantasmal Warrior",
+        "Multiple Projections",
         """
-            Place an ::token_illusion:: illusion token in radius. **This**
-            **round:** While you are performing actions, the
-            illusion tokens count as friendly melee minions.
+            Place up to 2 ::token_illusion:: Illusion tokens in radius.
+            You may swap with an Illusion token in play.
+        """.trimIndent(),
+        Color.GREEN,
+        level = 2,
+        variant = 1 to 2,
+        initiative = 5,
+        primaryAction = Action.SKILL,
+        modifier = Modifier.AREA,
+        modifierValue = 3,
+        secondaryMovement = 2,
+        secondaryDefense = 3,
+        item = Item.INITIATIVE,
+    ),
+    Card(
+        "Illusionary Force",
+        """
+            Place up to 2 ::token_illusion:: Illusion tokens in radius.
+            **This round:** While you are performing
+            actions, all Illusion tokens count as both
+            tokens and friendly melee minions.
         """.trimIndent(),
         Color.GREEN,
         level = 2,
@@ -231,114 +228,112 @@ val nebkher = setOf(
         initiative = 5,
         primaryAction = Action.SKILL,
         modifier = Modifier.AREA,
-        modifierValue = 3,
+        modifierValue = 4,
         secondaryMovement = 2,
-        secondaryDefense = 4,
+        secondaryDefense = 3,
         item = Item.ATTACK,
     ),
     Card(
         "An Illusion of Choice",
         """
-            A hero in radius must swap their unresolved
-            card with another card in their hand, if able.
-            May repeat once on the same Hero.
+            Name a color. **Next turn, after playing cards:**
+            Up to two enemy heroes in radius each
+            discard a card of that color, if able.
         """.trimIndent(),
         Color.BLUE,
         level = 3,
         variant = 1 to 2,
-        initiative = 10,
+        initiative = 11,
         primaryAction = Action.SKILL,
         modifier = Modifier.AREA,
         modifierValue = 4,
         secondaryMovement = 2,
-        secondaryDefense = 6,
-        item = Item.DEFENSE,
-    ),
-    Card(
-        "Master of Puppets",
-        """
-            **Choose one, or both**, in any order **—**
-            >>Before the attack: Move a friendly minion in
-            >range up to 4 spaces to a space adjacent to
-            >an enemy hero in range. Target that hero.
-            >>Target a minion adjacent to you.
-        """.trimIndent(),
-        Color.RED,
-        level = 3,
-        variant = 1 to 2,
-        initiative = 8,
-        primaryAction = Action.ATTACK,
-        primaryValue = 4,
-        modifier = Modifier.RANGE,
-        modifierValue = 3,
-        secondaryMovement = 3,
-        secondaryDefense = 8,
-        item = Item.MOVEMENT,
-    ),
-    Card(
-        "Master Of Illusions",
-        """
-            Place up to 3 ::token_illusion:: illusion tokens in radius.
-            **This round:** After an action makes you
-            discard a card, swap with an illusion token,
-            regardless of radius, then remove that token.
-        """.trimIndent(),
-        Color.GREEN,
-        level = 3,
-        variant = 1 to 2,
-        initiative = 5,
-        primaryAction = Action.SKILL,
-        modifier = Modifier.AREA,
-        modifierValue = 3,
-        secondaryMovement = 2,
         secondaryDefense = 5,
-        item = Item.INITIATIVE,
+        item = Item.MOVEMENT,
     ),
     Card(
         "Shift Reality",
         """
-            Split the board with a straight line of spaces
-            drawn through your space: **This turn:** You are
-            immune to actions of units on either side of
-            the line. All other units are immune to actions
-            of units on the opposite side of the line.
+            Split the board into two sides with a straight
+            line of spaces drawn through your space.
+            **This turn:** Units on either side of the line
+            cannot interact with you, nor with objects
+            and spaces on the other side of the line,
+            as if they did not exist.
         """.trimIndent(),
         Color.BLUE,
         level = 3,
         variant = 2 to 2,
-        initiative = 10,
+        initiative = 11,
         primaryAction = Action.SKILL,
         secondaryMovement = 2,
-        secondaryDefense = 6,
-        item = Item.ATTACK,
+        secondaryDefense = 5,
+        item = Item.RANGE,
     ),
     Card(
-        "Devious Scheme",
+        "Phantasmal Champion",
         """
-            Target a unit in range. After the attack:
-            If you target a hero, you may swap
-            any two of their resolved, unresolved,
-            or discarded cards.
+            **Choose one —**
+            >>Before the attack: Move an ::token_illusion:: Illusion token
+            >in range up to 2 spaces to a space adjacent
+            >to an enemy hero in range. Target that hero.
+            >>Target a unit adjacent to you.
         """.trimIndent(),
         Color.RED,
         level = 3,
-        variant = 2 to 2,
+        variant = 1 to 2,
         initiative = 8,
         primaryAction = Action.ATTACK,
-        primaryValue = 5,
+        primaryValue = 3,
         modifier = Modifier.RANGE,
-        modifierValue = 2,
+        modifierValue = 5,
         secondaryMovement = 3,
         secondaryDefense = 7,
         item = Item.AREA,
     ),
     Card(
-        "Phantasmal Colossus",
+        "Devious Scheme",
         """
-            Place an ::token_illusion:: illusion token in radius. **This**
-            **round:** Illusion tokens are immune to enemy
-            actions. While you are performing actions, the
-            illusion tokens count as friendly melee minions.
+            Target a unit in range. After the attack:
+            You may swap an enemy unit in range
+            with an ::token_illusion:: Illusion token in range.
+        """.trimIndent(),
+        Color.RED,
+        level = 3,
+        variant = 2 to 2,
+        initiative = 8,
+        primaryAction = Action.ATTACK,
+        primaryValue = 4,
+        modifier = Modifier.RANGE,
+        modifierValue = 2,
+        secondaryMovement = 3,
+        secondaryDefense = 6,
+        item = Item.DEFENSE,
+    ),
+    Card(
+        "Master of Illusions",
+        """
+            Place up to 3 ::token_illusion:: Illusion tokens in radius.
+            You may swap with an Illusion token in play.
+        """.trimIndent(),
+        Color.GREEN,
+        level = 3,
+        variant = 1 to 2,
+        initiative = 5,
+        primaryAction = Action.SKILL,
+        modifier = Modifier.AREA,
+        modifierValue = 3,
+        secondaryMovement = 2,
+        secondaryDefense = 4,
+        item = Item.INITIATIVE,
+    ),
+    Card(
+        "Illusionary Army",
+        """
+            Place up to 3 ::token_illusion:: Illusion tokens in radius.
+            **This round:** While you are performing
+            actions, all Illusion tokens count as both
+            tokens and friendly melee minions.
         """.trimIndent(),
         Color.GREEN,
         level = 3,
@@ -346,9 +341,9 @@ val nebkher = setOf(
         initiative = 5,
         primaryAction = Action.SKILL,
         modifier = Modifier.AREA,
-        modifierValue = 3,
+        modifierValue = 4,
         secondaryMovement = 2,
-        secondaryDefense = 5,
-        item = Item.RANGE,
+        secondaryDefense = 4,
+        item = Item.ATTACK,
     ),
 )
